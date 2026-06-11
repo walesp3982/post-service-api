5798d431093ffd531316730546e4bc926887fb33
Walter <josuewalterespejoguarachi@gmail.com>
2026-06-11 10:49:34 -0400

    add basic configuration

---
+++ config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfig struct {
+	DatabaseUrl      string
+	JWTSecret        string
+	JWTAccessExpiry  int // In minutes
+	JWTRefreshExpiry int // In Hours
+}
+
+func getEnvVariable(key string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		panic("Cannot get env variable: " + value)
+	}
+	return value
+}
+
+func convertInt(value string) int {
+	convert, err := strconv.Atoi(value)
+	if err != nil {
+		panic(err)
+	}
+	return convert
+}
+
+func NewAppConfig() AppConfig {
+	godotenv.Load()
+
+	databaseUrl := getEnvVariable("DATABASE_URL")
+	jwtSecret := getEnvVariable("JWT_SECRET")
+	jwtAccessExpiry := convertInt(getEnvVariable("JWT_ACCESS_EXPIRY"))
+	jwtRefreshExpiry := convertInt(getEnvVariable("JWT_REFRESH_EXPIRY"))
+
+	return AppConfig{
+		DatabaseUrl:      databaseUrl,
+		JWTSecret:        jwtSecret,
+		JWTAccessExpiry:  jwtAccessExpiry,
+		JWTRefreshExpiry: jwtRefreshExpiry,
+	}
+}
