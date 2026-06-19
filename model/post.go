823c5efd1a79aa919be97075a143db494d1da2f1
Walter <josuewalterespejoguarachi@gmail.com>
2026-06-18 22:04:00 -0400

    add tags json to model

--- model/post.go
+++ model/post.go
@@ -8,16 +8,16 @@ import (
 )
 
 type Post struct {
-	Id          uuid.UUID `gorm:"type:uuid"`
-	UserId      uuid.UUID `gorm:"type:uuid;not null"`
-	Title       string    `gorm:"type:varchar(255);not null;index:idx_title"`
-	Slug        string    `gorm:"uniqueIndex;type:varchar(255);not null;idx_slug"`
-	Content     string
-	PublishedAt time.Time `gorm:"index:idx_published"`
-	CreatedAt   time.Time
-	UpdateAt    time.Time
-	DeleteAt    gorm.DeletedAt
-	User        *User `gorm:"constraint:OnDelete:CASCADE" json:"user,omitempty"`
+	Id          uuid.UUID      `gorm:"type:uuid" json:"id"`
+	UserId      uuid.UUID      `gorm:"type:uuid;not null" json:"user_id"`
+	Title       string         `gorm:"type:varchar(255);not null;index:idx_title" json:"title"`
+	Slug        string         `gorm:"uniqueIndex;type:varchar(255);not null;idx_slug" json:"slug"`
+	Content     string         `json:"content"`
+	PublishedAt time.Time      `gorm:"index:idx_published" json:"published_at"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdateAt    time.Time      `json:"update_at"`
+	DeleteAt    gorm.DeletedAt `json:"delete_at"`
+	User        *User          `gorm:"constraint:OnDelete:CASCADE" json:"user,omitempty"`
 }
 
 func NewPost(userId uuid.UUID, title string, slug string, content string) Post {
